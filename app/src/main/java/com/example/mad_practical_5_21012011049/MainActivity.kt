57d655f16fa003c08acc63bfce9135a2965600cc
mehabhatt <mehabhatt21@gmail.com>
2023-08-16 21:09:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_5_21012011049/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.mad_practical_5_21012011049
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.provider.MediaStore
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val browseButton = findViewById<Button>(R.id.browse_btn)
+        val editText: EditText = findViewById(R.id.editTextUrl)
+        browseButton.setOnClickListener {
+            openBrowser(editText.text.toString())
+        }
+        val callButton = findViewById<Button>(R.id.call_btn)
+        val editPhone: EditText = findViewById(R.id.editTextPhone)
+        callButton.setOnClickListener {
+            call(editPhone.text.toString())
+        }
+        val callLogButton: Button = findViewById(R.id.callLog_btn)
+        callLogButton.setOnClickListener {
+            callLog()
+        }
+        val galleryButton: Button = findViewById(R.id.gallery_btn)
+        galleryButton.setOnClickListener {
+            gallery()
+        }
+        val cameraButton: Button = findViewById(R.id.camera_btn)
+        cameraButton.setOnClickListener {
+            camera()
+        }
+
+    }
+
+    fun openBrowser(url: String) {
+        Intent(Intent.ACTION_VIEW, Uri.parse(url)).also { startActivity(it) }
+    }
+
+    fun call(number: String) {
+        Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:$number")).also { startActivity(it) }
+    }
+
+    fun callLog() {
+        Intent(Intent.ACTION_VIEW, Uri.parse("content://call_log/calls")).also{ startActivity(it) }
+    }
+
+    fun gallery() {
+        Intent(Intent.ACTION_PICK).setType("image/*").also { startActivity(it) }
+    }
+
+    fun camera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { startActivity(it) }
+    }
+
+    fun alarm() {
+
+    }
+}
+
